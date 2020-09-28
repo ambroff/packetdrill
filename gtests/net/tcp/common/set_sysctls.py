697845bb9dd44cbcab176fc399033967fe123bb5
Kyle Ambroff-Kao <kyle@ambroffkao.com>
2020-09-28 13:40:51 -0700

    Convert to Python3.

--- gtests/net/tcp/common/set_sysctls.py
+++ gtests/net/tcp/common/set_sysctls.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
-import commands
+import subprocess
 import os
 import sys
 
@@ -8,14 +8,14 @@ pppid = int(os.popen("ps -p %d -oppid=" % os.getppid()).read().strip())
 filename = '/tmp/sysctl_restore_%d.sh' % pppid
 
 restore_file = open(filename, 'w')
-print >> restore_file, '#!/bin/bash'
+print('#!/bin/bash', file=restore_file)
 
 for a in sys.argv[1:]:
   sysctl = a.split('=')
 
   # save current value
-  cur_val = commands.getoutput('cat ' + sysctl[0])
-  print >> restore_file, 'echo "%s" > %s' % (cur_val, sysctl[0])
+  cur_val = subprocess.getoutput('cat ' + sysctl[0])
+  print('echo "%s" > %s' % (cur_val, sysctl[0]), file=restore_file)
 
   # set new value
   cmd = 'echo "%s" > %s' % (sysctl[1], sysctl[0])
